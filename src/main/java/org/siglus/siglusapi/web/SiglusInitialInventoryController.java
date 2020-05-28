d58dcdd46c5f5fd4802261ba55f0f6a8bd3e6695
Mengxing Yuan <mengxing.yuan@thoughtworks.com>
2020-05-28 15:52:46 +0800

    [Mengxing Yuan] #N/A refactor: /api/siglusapi/initialInventory GET

---
+++ src/main/java/org/siglus/siglusapi/web/SiglusInitialInventoryController.java
@@ -0,0 +1,39 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.web;
+
+import java.util.UUID;
+import org.siglus.siglusapi.dto.InitialInventoryFieldDto;
+import org.siglus.siglusapi.service.SiglusPhysicalInventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/siglusapi/initialInventory")
+public class SiglusInitialInventoryController {
+
+  @Autowired
+  private SiglusPhysicalInventoryService siglusPhysicalInventoryService;
+
+  @GetMapping
+  public InitialInventoryFieldDto searchInitialInventoryField(@RequestParam UUID facility) {
+    return siglusPhysicalInventoryService.canInitialInventory(facility);
+  }
+
+}
