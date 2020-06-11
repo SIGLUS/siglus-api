a8ae2eab9ec5765be1ea5a242360657f359f3609
Ping He <he.ping@thoughtworks.com>
2020-06-11 17:14:54 +0800

    [pinghe]#148 add unit test

--- src/test/java/org/siglus/siglusapi/service/SiglusRequisitionServiceTest.java
+++ src/test/java/org/siglus/siglusapi/service/SiglusRequisitionServiceTest.java
@@ -15,11 +15,18 @@
 
 package org.siglus.siglusapi.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,7 +36,29 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.openlmis.requisition.domain.requisition.Requisition;
 import org.openlmis.requisition.domain.requisition.RequisitionLineItem;
 import org.openlmis.requisition.domain.requisition.RequisitionLineItemDataBuilder;
+import org.openlmis.requisition.domain.requisition.RequisitionStatus;
+import org.openlmis.requisition.dto.ApprovedProductDto;
+import org.openlmis.requisition.dto.BasicRequisitionTemplateDto;
+import org.openlmis.requisition.dto.FacilityDto;
+import org.openlmis.requisition.dto.MetadataDto;
+import org.openlmis.requisition.dto.ObjectReferenceDto;
+import org.openlmis.requisition.dto.OrderableDto;
+import org.openlmis.requisition.dto.ProgramDto;
+import org.openlmis.requisition.dto.RequisitionV2Dto;
+import org.openlmis.requisition.dto.SupervisoryNodeDto;
+import org.openlmis.requisition.dto.UserDto;
+import org.openlmis.requisition.dto.VersionObjectReferenceDto;
+import org.openlmis.requisition.errorhandling.ValidationResult;
 import org.openlmis.requisition.repository.RequisitionRepository;
+import org.openlmis.requisition.service.RequisitionService;
+import org.openlmis.requisition.service.referencedata.ApproveProductsAggregator;
+import org.openlmis.requisition.service.referencedata.SupervisoryNodeReferenceDataService;
+import org.openlmis.requisition.utils.RequisitionAuthenticationHelper;
+import org.openlmis.requisition.web.RequisitionController;
+import org.siglus.common.domain.RequisitionTemplateExtension;
+import org.siglus.common.repository.RequisitionTemplateExtensionRepository;
+import org.siglus.siglusapi.service.client.SiglusRequisitionRequisitionService;
+import org.slf4j.profiler.Profiler;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SiglusRequisitionServiceTest {
@@ -40,14 +69,55 @@ public class SiglusRequisitionServiceTest {
   @Mock
   private RequisitionRepository requisitionRepository;
 
+  @Mock
+  private SiglusRequisitionRequisitionService siglusRequisitionRequisitionService;
+
+  @Mock
+  private RequisitionTemplateExtensionRepository requisitionTemplateExtensionRepository;
+
+  @Mock
+  private RequisitionController requisitionController;
+
+  @Mock
+  private RequisitionService requisitionService;
+
+  @Mock
+  private RequisitionAuthenticationHelper authenticationHelper;
+
+  @Mock
+  private SupervisoryNodeReferenceDataService supervisoryNodeService;
+
   @InjectMocks
   private SiglusRequisitionService siglusRequisitionService;
 
   private UUID facilityId = UUID.randomUUID();
 
+  private UUID programId = UUID.randomUUID();
+
   private UUID orderableId = UUID.randomUUID();
 
   private UUID requisitionId = UUID.randomUUID();
+
+  private UUID templateId = UUID.randomUUID();
+
+  private UUID supervisoryNodeId = UUID.randomUUID();
+
+  private UUID productId1 = UUID.randomUUID();
+  private Long productVersion1 = new Long(1);
+  private UUID productId2 = UUID.randomUUID();
+  private Long productVersion2 = new Long(1);
+  private VersionObjectReferenceDto productVersionObjectReference1
+      = createVersionObjectReferenceDto(productId1, productVersion1);
+  private VersionObjectReferenceDto productVersionObjectReference2
+      = createVersionObjectReferenceDto(productId2, productVersion2);
+
+  private Profiler profiler = new Profiler("GET_REQUISITION_TO_APPROVE");
+
+  private UserDto userDto = new UserDto();
+
+  private ProgramDto programDto = new ProgramDto();
+
+  private FacilityDto facilityDto = new FacilityDto();
 
   @Test
   public void shouldActivateArchivedProducts() {
@@ -60,12 +130,113 @@ public class SiglusRequisitionServiceTest {
         .activateArchivedProducts(Sets.newHashSet(orderableId), facilityId);
   }
 
+  @Test
+  public void shouldSearchRequisitionByIdAndKeepApproverApprovedProduct() {
+    RequisitionV2Dto requisitionV2Dto = createRequisitionV2Dto();
+    Requisition requisition = createRequisition();
+    when(siglusRequisitionRequisitionService.searchRequisition(requisitionId))
+        .thenReturn(requisitionV2Dto);
+    when(requisitionTemplateExtensionRepository
+        .findByRequisitionTemplateId(templateId)).thenReturn(createTemplate());
+    when(requisitionController.getProfiler(
+        "GET_REQUISITION_TO_APPROVE", requisitionId)).thenReturn(profiler);
+    when(requisitionController.findRequisition(requisitionId, profiler)).thenReturn(requisition);
+    when(requisitionService
+        .validateCanApproveRequisition(any(), any())).thenReturn(ValidationResult.success());
+    when(authenticationHelper.getCurrentUser()).thenReturn(userDto);
+    when(requisitionController.findProgram(programId, profiler)).thenReturn(programDto);
+    when(requisitionController.findFacility(facilityId, profiler)).thenReturn(facilityDto);
+    when(requisitionService.getApproveProduct(any(), any(), any()))
+        .thenReturn(createApproveProductsAggregator());
+    when(supervisoryNodeService.findOne(supervisoryNodeId)).thenReturn(createSupervisoryNodeDto());
+
+    RequisitionV2Dto response = siglusRequisitionService.searchRequisition(requisitionId);
+
+    verify(siglusRequisitionRequisitionService).searchRequisition(requisitionId);
+    verify(requisitionTemplateExtensionRepository).findByRequisitionTemplateId(templateId);
+    verify(requisitionController).getProfiler("GET_REQUISITION_TO_APPROVE", requisitionId);
+    verify(requisitionController).findRequisition(requisitionId, profiler);
+    verify(requisitionService).validateCanApproveRequisition(requisition, userDto.getId());
+    verify(authenticationHelper).getCurrentUser();
+    verify(requisitionController).findProgram(null, profiler);
+    verify(requisitionController).findFacility(null, profiler);
+    verify(supervisoryNodeService).findOne(null);
+    verify(requisitionService).validateCanApproveRequisition(requisition, userDto.getId());
+
+    Set<VersionObjectReferenceDto> availableProducts = response.getAvailableProducts();
+    assertEquals(availableProducts.size(), 1);
+    assertTrue(availableProducts.contains(productVersionObjectReference2));
+    //    verify();
+  }
+
   private Requisition createRequisition() {
     RequisitionLineItem lineItem = new RequisitionLineItemDataBuilder()
         .withOrderable(orderableId, 1L)
         .build();
     Requisition requisition = new Requisition();
+    requisition.setId(requisitionId);
     requisition.setRequisitionLineItems(Lists.newArrayList(lineItem));
     return requisition;
+  }
+
+  private VersionObjectReferenceDto createVersionObjectReferenceDto(UUID id, Long version) {
+    return new VersionObjectReferenceDto(id, null, "orderables", version);
+  }
+
+  private RequisitionV2Dto createRequisitionV2Dto() {
+    Set<VersionObjectReferenceDto> products = new HashSet<>();
+    products.add(productVersionObjectReference1);
+    products.add(productVersionObjectReference2);
+
+    RequisitionV2Dto requisitionV2Dto = new RequisitionV2Dto();
+    ObjectReferenceDto facility = new ObjectReferenceDto(facilityId);
+    requisitionV2Dto.setFacility(facility);
+    ObjectReferenceDto program = new ObjectReferenceDto(programId);
+    requisitionV2Dto.setProgram(program);
+    requisitionV2Dto.setAvailableProducts(products);
+    requisitionV2Dto.setTemplate(createTemplateDto());
+    requisitionV2Dto.setStatus(RequisitionStatus.AUTHORIZED);
+    return requisitionV2Dto;
+  }
+
+  private BasicRequisitionTemplateDto createTemplateDto() {
+    BasicRequisitionTemplateDto templateDto = new BasicRequisitionTemplateDto();
+    templateDto.setId(templateId);
+    return templateDto;
+  }
+
+  private RequisitionTemplateExtension createTemplate() {
+    RequisitionTemplateExtension extension = new RequisitionTemplateExtension();
+    extension.setRequisitionTemplateId(templateId);
+    extension.setEnableConsultationNumber(true);
+    extension.setEnableKitUsage(true);
+    extension.setEnablePatientLineItem(true);
+    extension.setEnableProduct(true);
+    extension.setEnableRapidTestConsumption(true);
+    extension.setEnableRegimen(true);
+    extension.setEnableUsageInformation(true);
+    return extension;
+  }
+
+  private ApproveProductsAggregator createApproveProductsAggregator() {
+    MetadataDto meta = new MetadataDto();
+    meta.setVersionNumber(new Long(1));
+    OrderableDto orderable = new OrderableDto();
+    orderable.setId(productId2);
+    orderable.setMeta(meta);
+    ApprovedProductDto productDto = new ApprovedProductDto();
+    productDto.setId(UUID.randomUUID());
+    productDto.setMeta(meta);
+    productDto.setOrderable(orderable);
+    List<ApprovedProductDto> list = new ArrayList<>();
+    list.add(productDto);
+    ApproveProductsAggregator aggregator = new ApproveProductsAggregator(list, programId);
+    return aggregator;
+  }
+
+  private SupervisoryNodeDto createSupervisoryNodeDto() {
+    SupervisoryNodeDto nodeDto = new SupervisoryNodeDto();
+    nodeDto.setId(supervisoryNodeId);
+    return nodeDto;
   }
 }
