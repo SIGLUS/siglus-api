0f37f94dcbbf8c5097677aa67b0de8857f584f57
Minghao Wang <minghao.wang@thoughtworks.com>
2020-07-10 12:49:15 +0800

    [Minghao Wang] #330 add interface decalration;

--- src/main/java/org/siglus/siglusapi/web/SiglusProofOfDeliveryController.java
+++ src/main/java/org/siglus/siglusapi/web/SiglusProofOfDeliveryController.java
@@ -24,9 +24,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +47,7 @@ public class SiglusProofOfDeliveryController {
    */
   @ResponseStatus(HttpStatus.CREATED)
   @ResponseBody
-  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  @PutMapping("/{id}")
   public ProofOfDeliveryDto updateProofOfDelivery(@PathVariable("id") UUID proofOfDeliveryId,
       @RequestBody ProofOfDeliveryDto dto,
       OAuth2Authentication authentication) {
