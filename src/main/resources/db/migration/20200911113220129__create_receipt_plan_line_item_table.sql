efcc7aa2d4417c66bd534b39c5d09dbd5fe39298
xiu.zeng <1374040664@qq.com>
2020-09-11 19:33:47 +0800

    [zengxiu]#493 fix conflict

--- src/main/resources/db/migration/20200911113220129__create_receipt_plan_line_item_table.sql
+++ src/main/resources/db/migration/20200911113220129__create_receipt_plan_line_item_table.sql
@@ -2,7 +2,6 @@
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
 
-
 CREATE TABLE siglusintegration.receipt_plan_line_item (
     id uuid PRIMARY KEY,
     receiptplanid uuid NOT NULL,
