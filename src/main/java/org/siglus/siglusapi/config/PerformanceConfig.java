11b59bbcc57c5dfc2f0fa65efe55bcc8a77ab9fe
Minghao Wang <minghao.wang@thoughtworks.com>
2021-06-17 10:50:07 +0800

    [Minghao] #71 add log execution time aop

---
+++ src/main/java/org/siglus/siglusapi/config/PerformanceConfig.java
@@ -0,0 +1,50 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Aspect
+@Configuration
+@Profile("local")
+@Slf4j
+public class PerformanceConfig {
+
+  @Pointcut("execution(* org.siglus.siglusapi.service..*(..)))")
+  public void allServiceMethods() {
+    // pointcut
+  }
+
+  @Around("allServiceMethods()")
+  public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    long start = System.currentTimeMillis();
+
+    Object proceed = joinPoint.proceed();
+
+    long executionTime = System.currentTimeMillis() - start;
+    if (executionTime > 100) {
+      log.warn("Bad performance: {} takes {} ms", joinPoint.getSignature(), executionTime);
+    }
+    return proceed;
+  }
+
+}
