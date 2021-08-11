f9aaa2ce4d6b49a11d7bc7378c924f1541f55dc2
Minghao Wang <minghao.wang@thoughtworks.com>
2021-08-11 11:21:23 +0800

    [Minghao] #180 android user check

---
+++ src/main/java/org/siglus/siglusapi/security/aop/AndroidUserCheck.java
@@ -0,0 +1,48 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.security.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.openlmis.stockmanagement.exception.PermissionMessageException;
+import org.openlmis.stockmanagement.util.Message;
+import org.siglus.siglusapi.util.AndroidHelper;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AndroidUserCheck {
+
+  private final AndroidHelper androidHelper;
+
+  @Pointcut("within(org.siglus.siglusapi.web.android.SiglusMeController)")
+  public void androidApi() {
+    // do nothing
+  }
+
+  @Before("androidApi()")
+  public void before() {
+    if (!androidHelper.isAndroid()) {
+      throw new PermissionMessageException(new Message("siglusapi.error.notAndroid"));
+    }
+  }
+
+}
