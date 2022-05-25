9b739354c89da3a7776d535657282a9ed5459de8
yinshangwei <swyin@thoughtworks.com>
2022-05-25 13:28:29 +0800

    [shangwei] #6 create metabase config table

---
+++ src/main/resources/db/migration/siglus/20220525052701477__create_metabase_config_table.sql
@@ -0,0 +1,12 @@
+-- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
+-- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
+-- Migrations should NOT BE EDITED. Add a new migration to apply changes.
+
+CREATE TABLE siglusintegration.metabase_config (
+    id uuid PRIMARY KEY,
+    level character varying(255),
+    dashboardid integer UNIQUE,
+    dashboardname character varying(255)
+);
+
+CREATE UNIQUE INDEX metabase_config_dashboardid_key ON siglusintegration.metabase_config(dashboardid);
