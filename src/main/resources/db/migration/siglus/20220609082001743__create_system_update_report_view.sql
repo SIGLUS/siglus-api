9d72149cd6094e67c0ab6f7895b3b50ba4001c78
xiangfeifeng <xiangfei.feng@thoughtworks.com>
2022-06-09 16:25:06 +0800

    [fengxiangfei] #7 update view for system update report

--- src/main/resources/db/migration/siglus/20220609082001743__create_system_update_report_view.sql
+++ src/main/resources/db/migration/siglus/20220609082001743__create_system_update_report_view.sql
@@ -1,7 +1,6 @@
 -- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-
 drop view if exists dashboard.vw_system_update_report;
 create view dashboard.vw_system_update_report as
 select
@@ -9,10 +8,10 @@ select
     fac.name as facilityname,
     ftm.category as facilitymergetype,
     ftm.facilitytypecode as facilitytype,
-    gz.code as provincecode,
-    gz.name as Provincename,
-    gz_dist.code as districtcode,
-    gz_dist."name" as districtname ,
+    gz_prov.code as provincecode,
+    gz_prov.name as Provincename,
+    gz.code as districtcode,
+    gz.name as districtname ,
     vfs.districtfacilitycode,
     vfs.provincefacilitycode,
     r.latestupdatetime,
@@ -37,8 +36,8 @@ from
             fac.id = r.facilityid
         left join referencedata.geographic_zones gz on
             gz.id = fac .geographiczoneid
-        left join referencedata.geographic_zones gz_dist on
-            gz_dist.id = gz.parentid
+        left join referencedata.geographic_zones gz_prov on
+            gz_prov.id = gz.parentid
         left join dashboard.vw_facility_supplier vfs on
             vfs.facilitycode = fac.code
         left join referencedata.facility_types ft on
