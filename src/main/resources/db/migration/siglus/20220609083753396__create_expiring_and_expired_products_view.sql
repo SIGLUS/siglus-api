a940cc81f81a6e66e56a567f67636a4c1dabcc89
ke.dang <ke.dang@thoughtworks.com>
2022-06-09 16:42:14 +0800

    [dangke] #8&#9 create view for expiring and expired products report

--- src/main/resources/db/migration/siglus/20220609083753396__create_expiring_and_expired_products_view.sql
+++ src/main/resources/db/migration/siglus/20220609083753396__create_expiring_and_expired_products_view.sql
@@ -2,28 +2,32 @@
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
 
-create view dashboard.expiring_product_report
+CREATE VIEW dashboard.vw_expiring_and_expired_products
             (provincename, districtname, facilitycode, facilityname, lotnumber, expirydate, productcode, proudctname,
              stockonhand, price, facilitytype, facilitymergetype, cmm, districtfacilitycode, provincefacilitycode,
-             withinonemouth)
+             expireinonemonth, hasexpiredformorethanonemonth)
 as
-SELECT pgz.name                                                                                     AS provincename,
-       gz.name                                                                                      AS districtname,
-       f.code                                                                                       AS facilitycode,
-       f.name                                                                                       AS facilityname,
-       l.lotcode                                                                                    AS lotnumber,
-       l.expirationdate                                                                             AS expirydate,
-       o.code                                                                                       AS productcode,
-       o.fullproductname                                                                            AS proudctname,
+SELECT pgz.name          AS provincename,
+       gz.name           AS districtname,
+       f.code            AS facilitycode,
+       f.name            AS facilityname,
+       l.lotcode         AS lotnumber,
+       l.expirationdate  AS expirydate,
+       o.code            AS productcode,
+       o.fullproductname AS proudctname,
        scoh.stockonhand,
-       po.priceperpack                                                                              AS price,
-       ft.code                                                                                      AS facilitytype,
-       ftm.category                                                                                 AS facilitymergetype,
+       po.priceperpack   AS price,
+       ft.code           AS facilitytype,
+       ftm.category      AS facilitymergetype,
        hfcmms.cmm,
        vfs.districtfacilitycode,
        vfs.provincefacilitycode,
-       (case when l.expirationdate between now() AND now() + INTERVAL '1 months' THEN 1 ELSE 0 END) AS withinonemouth
-
+       (CASE
+            WHEN l.expirationdate BETWEEN current_date AND current_date + INTERVAL '30 day' THEN 1
+            ELSE 0 END)  AS expireinonemonth,
+       (CASE
+            WHEN l.expirationdate < current_date - INTERVAL '30 day' THEN 1
+            ELSE 0 END)  AS hasexpiredformorethanonemonth
 FROM referencedata.facilities f
          LEFT JOIN stockmanagement.stock_cards sc ON f.id = sc.facilityid
          LEFT JOIN referencedata.facility_types ft ON ft.id = f.typeid
@@ -37,15 +41,30 @@ FROM referencedata.facilities f
                     WHERE NOT (EXISTS(SELECT 1
                                       FROM stockmanagement.calculated_stocks_on_hand scoh2
                                       WHERE scoh2.stockcardid = scoh1.stockcardid
-                                        AND scoh2.occurreddate > scoh1.occurreddate))) scoh ON sc.id = scoh.stockcardid
+                                        AND scoh2.processeddate > scoh1.processeddate))) scoh
+                   ON sc.id = scoh.stockcardid
          LEFT JOIN referencedata.geographic_zones gz ON gz.id = f.geographiczoneid
          LEFT JOIN referencedata.geographic_zones pgz ON gz.parentid = pgz.id
          LEFT JOIN referencedata.lots l ON l.id = sc.lotid
-         LEFT JOIN (SELECT DISTINCT ON (orderables.id) orderables.id,
-                                                       orderables.fullproductname,
-                                                       orderables.code
-                    FROM referencedata.orderables) o ON sc.orderableid = o.id
-         LEFT JOIN referencedata.program_orderables po ON o.id = po.orderableid
+         LEFT JOIN (SELECT o1.id,
+                           o1.fullproductname,
+                           o1.code
+                    FROM referencedata.orderables o1
+                    WHERE NOT (EXISTS(SELECT 1
+                                      FROM referencedata.orderables o2
+                                      WHERE o2.id = o1.id
+                                        AND o2.versionnumber > o1.versionnumber))) o ON sc.orderableid = o.id
+
+
+         LEFT JOIN (SELECT po1.id,
+                           po1.priceperpack,
+                           po1.orderableid
+                    FROM referencedata.program_orderables po1
+                    WHERE NOT (EXISTS(SELECT 1
+                                      FROM referencedata.program_orderables po2
+                                      WHERE po2.orderableid = po1.orderableid
+                                        AND po2.orderableversionnumber > po1.orderableversionnumber))) po
+                   ON sc.orderableid = po.orderableid
          LEFT JOIN (SELECT hfcmms1.cmm,
                            hfcmms1.facilitycode,
                            hfcmms1.productcode
@@ -56,8 +75,4 @@ FROM referencedata.facilities f
                                         AND hfcmms1.facilitycode = hfcmms2.facilitycode
                                         AND hfcmms1.periodend < hfcmms2.periodend))) hfcmms
                    ON hfcmms.productcode = o.code AND hfcmms.facilitycode = f.code
-
-where l.expirationdate between now() AND now() + INTERVAL '6 months';
-
-
-
+WHERE l.expirationdate <= current_date + INTERVAL '180 day';
