bd44fa711cc0da187fa2d6054465d95db4dc8724
rengang.kou <rengang.kou@thoughtworks.com>
2022-06-30 15:24:53 +0800

    [rengang.kou] #101 add get data task

---
+++ src/main/java/org/siglus/siglusapi/service/task/RequisitionReportTaskService.java
@@ -0,0 +1,60 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.service.task;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openlmis.requisition.dto.FacilityDto;
+import org.openlmis.requisition.dto.ProcessingPeriodDto;
+import org.openlmis.requisition.dto.ProgramDto;
+import org.openlmis.requisition.service.referencedata.FacilityReferenceDataService;
+import org.openlmis.requisition.service.referencedata.PeriodReferenceDataService;
+import org.openlmis.requisition.service.referencedata.ProgramReferenceDataService;
+import org.siglus.siglusapi.domain.RequisitionMonthlyReport;
+import org.siglus.siglusapi.repository.RequisitionMonthReportRepository;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RequisitionReportTaskService {
+
+  private final ProgramReferenceDataService programDataService;
+  private final FacilityReferenceDataService facilityReferenceDataService;
+  private final PeriodReferenceDataService periodReferenceDataService;
+  private final RequisitionMonthReportRepository requisitionMonthReportRepository;
+
+  public void refresh() {
+    List<ProgramDto> allProgramDto = programDataService.findAll();
+    log.info("allProgramDto.size = " + allProgramDto.size());
+    List<FacilityDto> allFacilityDto = facilityReferenceDataService.findAll();
+    log.info("allFacilityDto.size = " + allFacilityDto.size());
+    List<ProcessingPeriodDto> allProcessingPeriodDto = periodReferenceDataService.findAll();
+    log.info("allProcessingPeriodDto.size = " + allProcessingPeriodDto.size());
+
+    UUID uuid = UUID.fromString("2ee6bbf4-cfcf-11e9-9535-0242ac130005");
+    List<RequisitionMonthlyReport> facilities = requisitionMonthReportRepository.findByFacilityId(uuid);
+    log.info("facilities.size = " + facilities.size());
+
+
+
+
+  }
+
+
+}
