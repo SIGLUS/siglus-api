3b4a717ed09ce4fe767e0e091083d63c2a31dd5d
xiangfeifeng <xiangfei.feng@thoughtworks.com>
2022-07-12 17:43:58 +0800

    [fengxiangfei] #149 fix put requisition bug

--- src/main/java/org/siglus/siglusapi/dto/AgeGroupLineDto.java
+++ src/main/java/org/siglus/siglusapi/dto/AgeGroupLineDto.java
@@ -33,6 +33,7 @@ import org.springframework.beans.BeanUtils;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AgeGroupLineDto extends BaseDto {
+  private UUID id;
 
   private UUID requisitionId;
 
