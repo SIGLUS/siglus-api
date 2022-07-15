4311c987dfec77cbf9a35be127ca49900867ff8f
ke.dang <ke.dang@thoughtworks.com>
2022-07-15 15:46:11 +0800

    [dangke] add the generating logic of tracer drug persistent data

---
+++ src/main/java/org/siglus/siglusapi/service/task/report/TracerDrugTaskService.java
@@ -0,0 +1,34 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.service.task.report;
+
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.siglus.siglusapi.repository.TracerDrugRepository;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TracerDrugTaskService {
+
+  private final TracerDrugRepository tracerDrugRepository;
+
+  public void refreshTracerDrugPersistentData(LocalDate startDate, LocalDate endDate) {
+    tracerDrugRepository.insertDataWithinSpecifiedTime(startDate, endDate);
+  }
+}
---
+++ src/main/java/org/siglus/siglusapi/web/report/TracerDrugTaskController.java
@@ -0,0 +1,54 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.web.report;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import lombok.RequiredArgsConstructor;
+import org.siglus.siglusapi.service.task.report.TracerDrugTaskService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/siglusapi/task/tracerDrug")
+public class TracerDrugTaskController {
+
+  private final TracerDrugTaskService tracerDrugTaskService;
+
+  @Value("${tracer.drug.initialize.date}")
+  private final String tracerDrugInitializeDate;
+
+  private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+  @PostMapping("/refresh")
+  public ResponseEntity<String> refresh(LocalDate startDate, LocalDate endDate) {
+    tracerDrugTaskService.refreshTracerDrugPersistentData(startDate, endDate);
+    return ResponseEntity.ok("ok");
+  }
+
+  @PostMapping("/initialize")
+  public ResponseEntity<String> initialize() {
+    tracerDrugTaskService.refreshTracerDrugPersistentData(LocalDate.parse(tracerDrugInitializeDate, dateTimeFormatter),
+        LocalDate.now());
+    return ResponseEntity.ok("ok");
+  }
+
+}
