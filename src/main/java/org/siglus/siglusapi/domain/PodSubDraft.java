c9ae68541cfb7bc3e4f5911091ca47e7806590c2
siqi <siqi.wang@thoughtworks.com>
2022-07-25 17:49:02 +0800

    #138 add flyway and entity for pod sub draft and pod line items extension

---
+++ src/main/java/org/siglus/siglusapi/domain/PodSubDraft.java
@@ -0,0 +1,43 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.domain;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.siglus.common.domain.BaseEntity;
+import org.siglus.siglusapi.dto.enums.PodSubDraftEnum;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pod_sub_draft", schema = "siglusintegration")
+public class PodSubDraft extends BaseEntity {
+
+  private Integer number;
+  private PodSubDraftEnum status;
+  @Column(name = "operatorid")
+  private UUID operatorId;
+  @Column(name = "proofofdeliveryid")
+  private UUID proofOfDeliveryId;
+}
