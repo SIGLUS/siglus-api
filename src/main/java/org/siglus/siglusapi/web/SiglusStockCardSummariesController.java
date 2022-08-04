7491dbd30d2f97f83bd1774cb37928e638f172f3
peace <peace940814202@gmail.com>
2022-08-04 16:13:54 +0800

    [ping.he] #246 fix pipeline

--- src/main/java/org/siglus/siglusapi/web/SiglusStockCardSummariesController.java
+++ src/main/java/org/siglus/siglusapi/web/SiglusStockCardSummariesController.java
@@ -27,7 +27,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/siglusapi/stockCardSummaries")
