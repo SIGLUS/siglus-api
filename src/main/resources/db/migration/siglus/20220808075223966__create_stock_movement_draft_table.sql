72fca4caa9a7c820e62297d2481425678a8825c1
ZhongZhong <zhong.zhong@thoughtworks.com>
2022-08-08 16:30:09 +0800

    [zhong.zhong] #239 add migration script

---
+++ src/main/resources/db/migration/siglus/20220808075223966__create_stock_movement_draft_table.sql
@@ -0,0 +1,34 @@
+-- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
+-- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
+-- Migrations should NOT BE EDITED. Add a new migration to apply changes.
+CREATE TABLE siglusintegration.stock_movement_drafts
+(
+    id UUID PRIMARY KEY NOT NULL,
+    facilityid UUID NOT NULL,
+    programid UUID NOT NULL,
+    createddate DATE,
+    signature VARCHAR(255),
+    userid UUID
+);
+CREATE TABLE siglusintegration.stock_movement_draft_line_items
+(
+    id UUID PRIMARY KEY NOT NULL,
+    orderableid UUID NOT NULL,
+    productname text,
+    productcode text,
+    lotid UUID,
+    lotcode text,
+    locationid UUID,
+    locationcode text,
+    createddate date,
+    expirationdate DATE,
+    quantity INTEGER,
+    stockonhand INTEGER,
+    stockmovementdraftId UUID NOT NULL
+);
+
+ALTER TABLE siglusintegration.stock_movement_draft_line_items ADD FOREIGN KEY (stockmovementdraftId) REFERENCES stock_movement_drafts (id);
+
+create Index movement_draft_index on siglusintegration.stock_movement_drafts(facilityid, programid, userid);
+
+
