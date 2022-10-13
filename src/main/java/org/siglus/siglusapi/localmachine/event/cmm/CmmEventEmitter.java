2c1ec53fd1978a2368bc9693894f16110b4a2fcf
siqi <siqi.wang@thoughtworks.com>
2022-10-13 18:00:56 +0800

    [siqi] #453 add cmm cdc

---
+++ src/main/java/org/siglus/siglusapi/localmachine/event/cmm/CmmEventEmitter.java
@@ -0,0 +1,48 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.localmachine.event.cmm;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.siglus.siglusapi.localmachine.EventPublisher;
+import org.siglus.siglusapi.localmachine.cdc.CdcListener;
+import org.siglus.siglusapi.localmachine.cdc.CdcRecord;
+import org.siglus.siglusapi.localmachine.cdc.CdcRecordMapper;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Profile("localmachine")
+public class CmmEventEmitter implements CdcListener {
+
+  private final EventPublisher eventPublisher;
+  private final CdcRecordMapper cdcRecordMapper;
+
+  @Override
+  public String[] acceptedTables() {
+    return new String[]{
+        "siglusintegration.hf_cmms"
+    };
+  }
+
+  @Transactional
+  @Override
+  public void on(List<CdcRecord> records) {
+    eventPublisher.emitNonGroupEvent(new CmmEvent(cdcRecordMapper.buildEvents(records)));
+  }
+}
