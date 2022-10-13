15b258834ba158a22384505afcdd7a7cce4950c6
yinshangwei <swyin@thoughtworks.com>
2022-10-13 12:34:29 +0800

    [shangwei] #488 clear lots which are expired and SOH = 0 in all facilities for more than 6 months

---
+++ src/main/java/org/siglus/siglusapi/scheduledtask/ClearExpiredLotsTask.java
@@ -0,0 +1,40 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.scheduledtask;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.siglus.siglusapi.repository.LotManagementRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+@Profile("!localmachine")
+public class ClearExpiredLotsTask {
+
+  private final LotManagementRepository lotManagementRepository;
+
+  @Scheduled(cron = "${clear.expired.lots.cron}", zone = "${time.zoneId}")
+  @SchedulerLock(name = "clear_expiredLots_task")
+  public void clear() {
+    log.info("clear lots which are expired and SOH = 0 in all facilities for more than 6 months");
+    lotManagementRepository.clearExpiredLots();
+  }
+}
