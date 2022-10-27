d268d86fa9b30f06ebcfc62e6d790970c17b91c0
siqi <siqi.wang@thoughtworks.com>
2022-10-27 16:25:32 +0800

    [siqi] #N/A refactor: move method getPodSubDraftEnum()

--- src/main/java/org/siglus/siglusapi/web/request/OperateTypeEnum.java
+++ src/main/java/org/siglus/siglusapi/web/request/OperateTypeEnum.java
@@ -15,19 +15,7 @@
 
 package org.siglus.siglusapi.web.request;
 
-import org.siglus.siglusapi.dto.enums.PodSubDraftStatusEnum;
-
 public enum OperateTypeEnum {
   SAVE,
   SUBMIT;
-
-  public static PodSubDraftStatusEnum getPodSubDraftEnum(OperateTypeEnum operateEnum) {
-    switch (operateEnum) {
-      case SAVE:
-        return PodSubDraftStatusEnum.DRAFT;
-      case SUBMIT:
-      default:
-        return PodSubDraftStatusEnum.SUBMITTED;
-    }
-  }
 }
