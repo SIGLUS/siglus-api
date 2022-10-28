1b27e654ea7217a17acdb3324ae0d27a8bbf166c
ZhongZhong <zhong.zhong@thoughtworks.com>
2022-10-28 10:23:16 +0800

    zhong.zhong #392 add api to get current machine type.

---
+++ src/test/java/org/siglus/siglusapi/web/SiglusMachineTypeControllerTest.java
@@ -0,0 +1,45 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.siglus.siglusapi.web;
+
+import static org.mockito.Mockito.verify;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.siglus.siglusapi.localmachine.Machine;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SiglusMachineTypeControllerTest extends TestCase {
+
+  @InjectMocks
+  private SiglusMachineTypeController controller;
+
+  @Mock
+  private Machine machine;
+
+  @Test
+  public void shouldCallIsOnlineWeb() {
+    //when
+    controller.getCurrentMachineType();
+
+    //then
+    verify(machine).isOnlineWeb();
+  }
+}
