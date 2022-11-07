dd480dcc59475fc332ca3504b45cba759c044d5e
yinshangwei <swyin@thoughtworks.com>
2022-11-07 15:10:15 +0800

    [shangwei] #522 add "estimatedQuantity" column in requisition template.

--- src/main/java/org/siglus/siglusapi/service/SiglusRequisitionService.java
+++ src/main/java/org/siglus/siglusapi/service/SiglusRequisitionService.java
@@ -1021,6 +1021,7 @@ public class SiglusRequisitionService {
           RequisitionLineItemV2Dto lineItemV2Dto = (RequisitionLineItemV2Dto) lineItem;
           lineItemV2Dto.setAuthorizedQuantity(itemExtension.getAuthorizedQuantity());
           lineItemV2Dto.setSuggestedQuantity(itemExtension.getSuggestedQuantity());
+          lineItemV2Dto.setEstimatedQuantity(itemExtension.getEstimatedQuantity());
           lineItemV2Dto.setExpirationDate(itemExtension.getExpirationDate());
         }
       });
