000c1998e8ce92d60c8f6142819d618d86384a31
siqi <siqi.wang@thoughtworks.com>
2022-11-15 18:42:49 +0800

    [siqi] #N/A fix test

--- src/test/java/org/siglus/siglusapi/localmachine/server/LocalExportImportServiceTest.java
+++ src/test/java/org/siglus/siglusapi/localmachine/server/LocalExportImportServiceTest.java
@@ -19,7 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyList;
-import static org.mockito.Matchers.anySet;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -110,7 +109,6 @@ public class LocalExportImportServiceTest {
     localExportImportService.exportEvents(httpServletResponse);
 
     // then
-    verify(siglusFacilityService).getFacilityIdToName(anySet());
     verify(eventStore).getEventsForExport(homeFacilityId);
   }
 
