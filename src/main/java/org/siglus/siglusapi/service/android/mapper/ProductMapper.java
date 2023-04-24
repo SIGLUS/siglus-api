2cde8f4667edc5297854edca35c1f9d9c25e69bd
PingHe <ping@trybuysocial.com>
2023-04-24 11:48:55 +0800

    [pinghe] #ticket-146 convert android category

--- src/main/java/org/siglus/siglusapi/service/android/mapper/ProductMapper.java
+++ src/main/java/org/siglus/siglusapi/service/android/mapper/ProductMapper.java
@@ -69,6 +69,9 @@ public interface ProductMapper {
   }
 
   default String convertOrderableCategory(String original) {
+    if (original == null) {
+      return "Default";
+    }
     switch (original) {
       case "Adulto":
         return "Adult";
