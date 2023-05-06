0ff0806c406f8435cf1c596825852ec00538c609
yinshangwei <swyin@thoughtworks.com>
2023-05-06 11:05:27 +0800

    [shangwei] #ticket-150 fix pmd

--- src/test/java/org/siglus/siglusapi/localmachine/eventstore/EventBackupTaskTest.java
+++ src/test/java/org/siglus/siglusapi/localmachine/eventstore/EventBackupTaskTest.java
@@ -17,7 +17,6 @@ package org.siglus.siglusapi.localmachine.eventstore;
 
 import static org.mockito.Matchers.anySetOf;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.when;
 
