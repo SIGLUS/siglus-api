26a09146ef0b732569d55cf692c6341c47f91bbc
PingHe <ping@trybuysocial.com>
2023-05-11 16:07:51 +0800

    [pinghe] Ticket#142 fix pipeline

--- src/main/java/org/siglus/siglusapi/web/SiglusOrderableController.java
+++ src/main/java/org/siglus/siglusapi/web/SiglusOrderableController.java
@@ -34,7 +34,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
