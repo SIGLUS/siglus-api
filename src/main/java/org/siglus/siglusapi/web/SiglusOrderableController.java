832ff9a4f4b6f967ebd31acde66392c502dc8af7
PingHe <ping@trybuysocial.com>
2023-05-11 11:26:38 +0800

    [pinghe] Ticket#142 add definition

--- src/main/java/org/siglus/siglusapi/web/SiglusOrderableController.java
+++ src/main/java/org/siglus/siglusapi/web/SiglusOrderableController.java
@@ -34,6 +34,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -87,7 +88,7 @@ public class SiglusOrderableController {
   }
 
 
-  @GetMapping("/unit")
+  @PostMapping("/unit")
   public List<ProgramOrderablesExtensionDto>  getUnitByOrderableIds(@RequestBody List<UUID> orderableIds) {
     return orderableService.getUnitByOrderableIds(orderableIds);
   }
