57f41fc7c079d47bc6fe1d6229858db401c38518
Kai Feng <kai.feng@thoughtworks.com>
2024-06-19 16:02:08 +0800

    [Kai FENG] #13 fix flyway

--- src/main/resources/db/migration/siglus/202406190733090880__update_pod_sub_draft_line_items.sql
+++ src/main/resources/db/migration/siglus/202406190733090880__update_pod_sub_draft_line_items.sql
@@ -1,4 +1,8 @@
 -- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-ALTER TABLE siglusintegration.pod_sub_draft_line_items ALTER COLUMN rejectionreasonid TYPE uuid USING rejectionreasonid::uuid::uuid;
+alter table siglusintegration.pod_sub_draft_line_items
+    drop column rejectionreasonid;
+
+alter table siglusintegration.pod_sub_draft_line_items
+    add rejectionreasonid uuid;
