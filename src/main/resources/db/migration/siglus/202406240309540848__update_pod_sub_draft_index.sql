d4121d49a2453ad0ba31d493a439f980c14bbdcc
Kai Feng <kai.feng@thoughtworks.com>
2024-06-24 13:15:57 +0800

    [Kai FENG] #13 update for pod draft

---
+++ src/main/resources/db/migration/siglus/202406240309540848__update_pod_sub_draft_index.sql
@@ -0,0 +1,8 @@
+-- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
+-- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
+-- Migrations should NOT BE EDITED. Add a new migration to apply changes.
+DROP INDEX siglusintegration.pod_sub_draft_line_items_by_location_podlineitemid_idx;
+DROP INDEX siglusintegration.pod_sub_draft_line_items_by_location_id_uindex;
+CREATE INDEX pod_sub_draft_line_items_by_location_podlineitemid_idx ON siglusintegration.pod_sub_draft_line_items_by_location (podlineitemid);
+
+CREATE INDEX pod_sub_draft_line_items_podsubdraftid_idx ON siglusintegration.pod_sub_draft_line_items (podsubdraftid);
