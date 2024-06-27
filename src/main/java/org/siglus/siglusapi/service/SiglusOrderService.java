91b84fd8d720758f8d51a87e11c35acd8080b8fa
pinghe <pinghe@Pings-MacBook-Pro.local>
2024-06-27 08:57:03 -0700

    [pinghe] #N/A add log

--- src/main/java/org/siglus/siglusapi/service/SiglusOrderService.java
+++ src/main/java/org/siglus/siglusapi/service/SiglusOrderService.java
@@ -1040,6 +1040,7 @@ public class SiglusOrderService {
   private void setIfIsKit(SiglusOrderDto siglusOrderDto) {
     List<UUID> orderableIds = siglusOrderDto.getOrder().orderLineItems().stream()
         .map(orderableDto -> orderableDto.getOrderable().getId()).collect(Collectors.toList());
+    log.info("setIfIsKit orderId: {}", siglusOrderDto.getOrder().getId());
     List<Orderable> orderables = orderableRepository.findLatestByIds(orderableIds);
     Map<UUID, Boolean> orderableToIsKitMap = new HashMap<>();
     orderables.forEach(orderable ->
