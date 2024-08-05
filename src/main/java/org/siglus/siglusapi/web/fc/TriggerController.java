02221d8d3dd42bafd772e2aa6018a572cba1e6ce
yinshangwei <swyin@thoughtworks.com>
2024-08-05 15:37:13 +0800

    [shangwei] #N/A add @Profile("!localmachine") for TriggerController

--- src/main/java/org/siglus/siglusapi/web/fc/TriggerController.java
+++ src/main/java/org/siglus/siglusapi/web/fc/TriggerController.java
@@ -19,12 +19,14 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.siglus.siglusapi.service.fc.FcScheduleService;
 import org.siglus.siglusapi.validator.FcQueryDate;
+import org.springframework.context.annotation.Profile;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("!localmachine")
 @RestController
 @RequestMapping("/api/siglusapi/fc")
 @RequiredArgsConstructor
