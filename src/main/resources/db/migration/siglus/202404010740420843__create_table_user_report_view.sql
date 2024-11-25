7ca9fb7b1c2ab342ddd076fbf057b47998501611
yinshangwei <swyin@thoughtworks.com>
2024-11-25 20:06:10 +0800

    [shangwei] #N/A update schema to avoid re-run error

--- src/main/resources/db/migration/siglus/202404010740420843__create_table_user_report_view.sql
+++ src/main/resources/db/migration/siglus/202404010740420843__create_table_user_report_view.sql
@@ -1,8 +1,7 @@
 -- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-DROP TABLE IF EXISTS siglusintegration.user_report_view;
-CREATE TABLE siglusintegration.user_report_view
+CREATE TABLE IF NOT EXISTS siglusintegration.user_report_view
 (
     id                     uuid PRIMARY KEY,
     userid                 uuid NOT NULL,
--- src/main/resources/db/migration/siglus/202404190333290992__create_table_physical_inventories_histories.sql
+++ src/main/resources/db/migration/siglus/202404190333290992__create_table_physical_inventories_histories.sql
@@ -1,8 +1,7 @@
 -- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-DROP TABLE IF EXISTS siglusintegration.physical_inventories_histories;
-CREATE TABLE siglusintegration.physical_inventories_histories
+CREATE TABLE IF NOT EXISTS siglusintegration.physical_inventories_histories
 (
     id            uuid PRIMARY KEY,
     facilityid    uuid                     NOT NULL,
