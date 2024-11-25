2b95c955fc849becd16e76b1d51957cd5f467c0b
yinshangwei <swyin@thoughtworks.com>
2024-11-25 19:52:50 +0800

    [shangwei] #N/A update schema to avoid re-run error

--- src/main/resources/db/migration/siglus/202403020342240001__create_index_requisitions_modifieddate_and_pod_recived_date.sql
+++ src/main/resources/db/migration/siglus/202403020342240001__create_index_requisitions_modifieddate_and_pod_recived_date.sql
@@ -2,8 +2,8 @@
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
 
-create index requisitions_modifieddate_index
+create index IF NOT EXISTS requisitions_modifieddate_index
     on requisition.requisitions (modifieddate);
 
-create index proofs_of_delivery_receiveddate_index
+create index IF NOT EXISTS proofs_of_delivery_receiveddate_index
     on fulfillment.proofs_of_delivery (receiveddate);
