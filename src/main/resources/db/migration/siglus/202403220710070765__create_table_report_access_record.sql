d659899004b22469c6687cefdf2d3ec7003f43fd
yinshangwei <swyin@thoughtworks.com>
2024-11-25 20:03:29 +0800

    [shangwei] #N/A update schema to avoid re-run error

--- src/main/resources/db/migration/siglus/202403220710070765__create_table_report_access_record.sql
+++ src/main/resources/db/migration/siglus/202403220710070765__create_table_report_access_record.sql
@@ -1,8 +1,7 @@
 -- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
 -- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
 -- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-DROP TABLE IF EXISTS siglusintegration.report_access_record;
-CREATE TABLE siglusintegration.report_access_record
+CREATE TABLE IF NOT EXISTS siglusintegration.report_access_record
 (
     id                     uuid PRIMARY KEY,
     userid                 uuid NOT NULL,
